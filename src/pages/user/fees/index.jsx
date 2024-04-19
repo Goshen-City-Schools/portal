1160789a6cfc79d9e2d9659b1e08a5f9c9019433
Mr. Paul Ishaili C <paulishaili@gmail.com>
2024-04-19 08:42:02 +0100

    Updated invoice generation page

--- src/pages/user/fees/index.jsx
+++ src/pages/user/fees/index.jsx
@@ -1,69 +1,25 @@
 import React from "react";
-import { useSearchParams } from "react-router-dom";
 
-import { Flex, Button, Box } from "@chakra-ui/react";
-import { SearchWidget } from "../../../widgets";
-
-import { MdAdd, MdInventory } from "react-icons/md";
-import IconComponent from "../../../components/Icon.component";
 import PageWrapper from "../../../components/PageWrapper";
 import PageSectionHeader from "../../../components/PageSectionHeader";
-import { useUser } from "../../../app/contexts/UserContext";
-import { useFees, useStudentFee } from "../../../hooks/";
+
+import GenerateInvoice from "../../../components/forms/GenerateInvoice.form";
 
 export default function FeesPage() {
-  const { feeType } = useSearchParams;
-  const { user: student } = useUser();
-
-  const { fees: feesData } = useFees(feeType);
-
   //   TODO: Set useStudentFee hook
 
   return (
     <PageWrapper>
       <PageSectionHeader
-        pageTitle={`${feeType} Fee`}
-        pageCrumb={`Home / Fees / ${feeType}`}
+        pageTitle={`Fees Invoice`}
+        pageCrumb={`Home / Fees / Generate Invoice`}
       />
-
-      {/*  */}
-      <Flex
-        justifyContent={"space-between"}
-        alignItems={"center"}
-        mt={8}
-        mb={6}
-      >
-        <SearchWidget height={10} text={"Search Fees"} />
-
-        <Flex gap={4} fontSize={"sm"}>
-          <Button bg={"brand.700"} size={"sm"} color={"neutral.100"}>
-            <IconComponent>
-              <MdInventory />
-            </IconComponent>
-            Payments History
-          </Button>
-
-          <Button bg={"brand.700"} size={"sm"} color={"neutral.100"}>
-            <IconComponent>
-              <MdAdd />
-            </IconComponent>
-            Generate Invoice
-          </Button>
-        </Flex>
-      </Flex>
 
       {/*  */}
 
       {/* Fee Detail Sheet */}
 
-      <Box
-        border={"1px solid"}
-        bg={"white"}
-        rounded={"lg"}
-        px={8}
-        py={4}
-        fontWeight={"bold"}
-      ></Box>
+      <GenerateInvoice />
     </PageWrapper>
   );
 }
